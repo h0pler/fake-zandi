5136634df643f15b630cfa4b188ba93dcb7de07f
h0pler <h0pler@proton.me>
2024-01-27 20:35:40 +0900

    minor fixes

--- commands.py
+++ commands.py
@@ -4,16 +4,12 @@ def command(command):
     print(f"Command : {command}")
     os.system(command)
 
-def clear():
-    os.system("cls")
-
 def git_init():
     if os.path.isdir(".git"):
         print(".git exist !!")
     else:
         print("Create git init")
         command("git init")
-        clear()
 
 def git_add():
     command("git add file.txt")
